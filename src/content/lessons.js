f35190cdea9e5206011fcb5563f9da8532d8bc15
Mehrdadmg <m.majidzadeh.g@gmail.com>
2024-05-31 14:04:47 +0200

    added new contents

--- src/content/lessons.js
+++ src/content/lessons.js
@@ -1,8 +1,14 @@
 import a1_u07 from './deutschSentences_a1_u07';
 import a1_u08 from './deutschSentences_a1_u08';
+import a1_u09 from './deutschSentences_a1_u09';
+import a1_u10 from './deutschSentences_a1_u10';
+import a1_u11 from './deutschSentences_a1_u11';
+import a1_u12 from './deutschSentences_a1_u12';
+import a1_u13 from './deutschSentences_a1_u13';
+
 import a1_u14 from './deutschSentences_a1_u14';
 
-const allLessonsContent = [a1_u07, a1_u08, a1_u14];
+const allLessonsContent = [a1_u07, a1_u08, a1_u09, a1_u10, a1_u11, a1_u12, a1_u13, a1_u14];
 const allLanguagesTranslate = ['english', 'russian', 'farsi'];
 
 const allLessonTitle = allLessonsContent.map((lesson) => {
